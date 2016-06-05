4f79efcb7dbafc70a6d8955c60fbceecd20d1165
Jerry Meng <ymeng@umich.edu>
2016-06-05 15:20:42 -0400

    hopefully this deploys properly

--- FastForum/settings.py
+++ FastForum/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
